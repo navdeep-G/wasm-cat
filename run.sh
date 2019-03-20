eb8565e042e59603772e7dd9819a8087803521e4
navdeep-G <mr.navdeepgill@gmail.com>
2019-03-20 10:21:34 -0700

    Add html/package files

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+url="http://localhost:8080"
+
+source ../emsdk/emsdk_set_env.sh 
+
+emcc --bind -Oz cat.cc -o cat.js \
+-s WASM=1 -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['addOnPostRun']" \
+|| exit 1
+
+case "$OSTYPE" in
+  msys)
+    start $url;;
+  cygwin)
+    cygstart $url;;
+  darwin*)
+    open $url;;
+  *)
+    xdg-open $url;;
+esac
